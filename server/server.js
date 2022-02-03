8cb9012f26098e073c45b27e4fc7ad06a9b20a75
josephdaw <josephdaw@icloud.com>
2022-02-03 11:26:49 +1030

    setting up Apollo server

--- server/server.js
+++ server/server.js
@@ -3,9 +3,25 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// Import the ApolloServer class
+const { ApolloServer } = require('apollo-server-express');
+
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Create a new instance of an Apollo server with the GraphQL schema
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// Update Express.js to use Apollo server features
+server.applyMiddleware({ app });
+
+// querystring library when false or the qs library when true. 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
