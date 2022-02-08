554382087b6cbe2a70cff1679fd537812e28aecb
josephdaw <josephdaw@icloud.com>
2022-02-08 15:31:14 +1030

    refactoring signup form

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -17,7 +18,8 @@ const SignupForm = () => {
     setUserFormData({ ...userFormData, [name]: value });
   };
 
-  const [addUser, { error, data }] = useMutation(ADD_USER);
+  const [addUser, { error, addUserData }] = useMutation(ADD_USER);
+  console.log(error)
   
   const handleFormSubmit = async (event) => {
     event.preventDefault();
